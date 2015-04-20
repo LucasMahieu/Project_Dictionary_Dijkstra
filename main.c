2c258b36a3a20a3b812d119febddcb12c4b1494e
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-04-20 15:37:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,9 +12,16 @@
 int main (FILE* f,int taille_mots){  // le main va prendre 2 arg. le fichier de mots et la tailles max des mots
 	T_GRAPHE G;
 	
-	G.table=creer_graphe(f,&(G.taille));   // ici, le scelette du graphe est creer = tous les mots sont rangés selon le hachoire
+	G.table=creer_graphe(f,&(G.taille));   // ici, le squelette du graphe est créé = tous les mots sont rangés selon le hachoire
 	
-// Mtn il faut creer les liste de successeurs ...
+/*Création de la liste de successeur
+	On prend un mot on change une lettre et on regarde si le nouveau mot appartient au fichier. 
+	Si c'est le cas on ajoute ce mot en successeur avec un ajout tête. Il faut parcourrir l'ensemble 
+	des élements de la table de hashage*/
+	
+	
+	
+
 	
 	
 	
