eca7c936f46889d6c61c46124f88bb024a7d2fd3
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-04-20 15:02:28 +0200

    Update Test_Table_Hashage.c

--- Sources/Test_Table_Hashage.c
+++ Sources/Test_Table_Hashage.c
@@ -1,5 +1,21 @@
 
 #include "main.h" 
 
+void main(FILE *f){ // le main va prendre 2 arg. le fichier de mots et la tailles max des mots
+  	T_GRAPHE G;
+  	int n; 
+	
+  	G.table=creer_graphe(f,&(G.taille));  
+	
+	  while(1){
+	
+    	printf("Quelle valeur pour n ? \n");
+    	scanf("%d",&n); /*On se a place à un certain point dans la table de hashage et on affiche l'ensemble de la liste associée*/
 
+     visualiser_liste(G.table + n);
+     
+    getchar();
+	  }
+
+}
 
