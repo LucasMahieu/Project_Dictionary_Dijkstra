495e3b5711ef1c880ccfe243c9c0c2d59df9c7b9
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-04-27 14:47:04 +0200

    Update graphe.c

--- Sources/graphe.c
+++ Sources/graphe.c
@@ -39,42 +39,36 @@ void recherche_successeur(T_GRAPHE G,int taille_mot){
 	int i = 0;
 	int j = 0;
 	int m = 0;
+	int Position = 0;
 	char mot_remplissage;
 	char alphabet = "abcdefghijklmnopqrstuvwxyz" /*On utilisera cette chaine de caractère pour mofifier les charactères 
 	dans le mot "mot"*/
 	L_SOMMET* p = NULL;/* On prend un pointeur pour pouvoir parcourir les listes associées à la table de hachage*/
+	L_SOMMET* q = NULL;
+	q = G->Table;
+	T_SOMMET* r = NULL;
 	
 	FILE* f1 = fopen(fichier,"r");    // On ouvre le fichier dans f1
 	if (f1==NULL) return 1;
 	
-	/*On initialise un mot auquel on changera toutes les lettres. La taille de ce mot dépend du fichier texte ouvert, on prendra la variable taille_mot*/
-	for(i=0,i<taille_mot,i++){
-		s[i]='a';//Je ne suis pas certain de cette initialisation
-	}
+	/*On parcours la table de hashage pour chaque mot formé avec la variable "mot_remplissage". Ce mot "mot_remplisssage" est formé à partir 
+	de des mots déjà présent dans la table de hashage. Et si on trouve ce nouveau mot_remplissage dans la table de hashage on peut l'ajouter en tête*/
 	
-	/*On parcours la table de hashage pour chaque mot formé avec la variable "mot_remplissage", et on regarde si ce nouveau mot_remplissage différe
-	d'une lettre avec le sommet auquel on s'intéresse. Si c'est le cas on ajout en tête ce mot_remplissage formé à la liste de 
-	successeur du mot en question. Il faut par ailleurs que le mot_remplissage modifié appartienne au fichier texte*/
-	
-	for(k=0,k<26,k++){
-		for(j=0, j<26,j++){/*26 correspond à l taille de l'aphabet que l'on doit parcourir*/
-			while (fgets(mot_fichier, taille_mots+2,f1)!=NULL){
-				if(strcmp(mot_fichier,mot_remplissage) == 0){/*Si le mot "mot_remplissage" appartient au fichier texte, on peut alors chercher à parcourir la table de hashage.*/
-					for(m=0,m<taille_table,m++){
-						p = Table_hachage[m];
-						while(p->suiv != NULL){
-						/*Il faut chercher à savoir il y a exactement une lettre de différence, on utilise la fonction du fichier "fontions.c"*/
-							if(difference_une_lettre(mot_remplissage,(p->valeur)->mot,taille_mot)==1){/*Il faut pointer sur le mot qui corresponf au valeur de T_SOMMET*/
-								ajout_tete(mot_remplissage,(p->valeur)->Liste_succ);/*On ajoute en tête si le mot est bien un métagramme*/
-							}
-						}
+	for(k=0,k<taille_mot,k++){
+		/*On initialise le "mot_remplissage"*/
+		for(m=0,m<taille_table,m++){
+		mot_remplissage=(p[m]->valeur)->mot;
+			for(j=0, j<26,j++){
+				mot_remplissage[k]=alphabet[j];	/*On modifie mot_remplissage avec le tableua alphabet*/
+				Position = hash(mot_remplissage,CODE_HACHAGE,nombre_mots);/*On hash le mot_remplissage et on regarde si il appartient à la table de hasgae*/
+					while(!est_vide(q[Position]){/*On parcours l'ensemble des L_SOMMET de la table de hashage*/
+					r = rech(mot_remplissage,(q[Position]->valeur));/*On recherche si le mot_remplissage est bien présent dans la tablde hashage*/
+						if(!r){/*Si mot_remplissage est présent dans la table de hashage, on peut faire un ajout en tête*/
+							ajout_tete(mot_remplissage,(p->valeur)->Liste_succ);}/*On ajoute en tête si le mot est bien un métagramme présent dans la table de hashage*/
+							q[Position]= q[Position]->suiv;
 					}
-				}
 			}
-		mot_remplissage[0]=alphabet[j];/*On modifie la dernière lettre de mot_remplissage, en utilisant alphabet*/
-		}	/*Mais comment faire pour avoir tous les cas ? On sait qu'il y aura 26^taille_mot combinaisons */
-		mot remplissage[1]=alphabet[k];
 	}
-
-}	
+}
+	
 
