f099c92bad1cff861a3666b04d254dd7b80c5b67
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-04-27 14:55:21 +0200

    Update fonctions.h

--- Headers/fonctions.h
+++ Headers/fonctions.h
@@ -10,7 +10,7 @@
 #define ____fonctions__
 
 int hash(char mot[], int a,int n);
-int difference_une_lettre(char* mot1, char* mot2,int taille_mot);
+void recherche_successeur(T_GRAPHE G,int taille_mot);
 
 
 
