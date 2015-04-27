94325d4978f46f625d2ce2b21d86df4dda0aa5ae
Lucas Mahieu <lucas.mahieu@icloud.com>
2015-04-27 15:16:48 +0200

    Lundi 27.04.15 LUCAS Bug include + types + mise en forme fichiers de travailles + probleme de type

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
-CFLAGS=-g -O2 
+CFLAGS=-g -O2 -I./Headers
 LDFLAGS= -lm
 
-Test_Table_Hashage: Test_Table_Hashage.o fonctions.o graphe.o liste_succ.o sommet.o succ.o
+Test_Table_Hashage: Test_Table_Hashage.o fonctions.o types.o liste.o
 	gcc -o $@ $^  $(LDFLAGS)
 
-
-%.o: %.c
-	gcc -c $(CFLAGS) $< 
+%.o: Sources/%.c
+	gcc -c $<  $(CFLAGS)
--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 //
 //
 
-#include "./Header/main.h"
+#include "./Headers/main.h"
 
 
 int main (FILE* f,int taille_mots){  // le main va prendre 2 arg. le fichier de mots et la tailles max des mots
 	T_GRAPHE G;
 	
-	G.table=creer_graphe(f,&(G.taille));   // ici, le scelette du graphe est creer = tous les mots sont rangés selon le hachoire
+	G.table = creer_graphe(f,&(G.taille));   // ici, le scelette du graphe est creer = tous les mots sont rangés selon le hachoire
 	
 // Mtn il faut creer les liste de successeurs ...
 	
