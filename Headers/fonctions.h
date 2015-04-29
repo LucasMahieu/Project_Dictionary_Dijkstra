be668ef9ad940b69d299c2804267359adc81433f
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-04-29 18:42:51 +0200

    Update fonctions.h

--- Headers/fonctions.h
+++ Headers/fonctions.h
@@ -10,7 +10,7 @@
 #define ____fonctions__
 
 int hash(char mot[], int a,int n);
-void recherche_successeur(T_GRAPHE G,int taille_mot);
+void recherche_successeur(T_GRAPHE G,int taille_mot, int nombre_mot,int taille_table);
 
 int taille_fichier(FILE* fichier); 
 
