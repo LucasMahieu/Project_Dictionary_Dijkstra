30a6b4814bd1f669702130edc824e6bf47439fe4
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-05-04 19:09:01 +0200

    Update fonctions.h
    maj 4/5

--- Headers/fonctions.h
+++ Headers/fonctions.h
@@ -9,13 +9,24 @@
 #ifndef ____fonctions__
 #define ____fonctions__
 
-int hash(char mot[], int a,int n);
-void recherche_successeur(T_GRAPHE G,int taille_mot, int nombre_mot,int taille_table);
+int Hash(char mot[], int a,int n,int taille_mots);
 
-int taille_fichier(FILE* fichier); 
+void Recherche_Successeur(T_GRAPHE G,FILE* f1,int taille_mot, int nombre_mots,int taille_table);
 
-L_SOMMET* creer_graphe(char* fichier,int* taille_table,int taille_mots);
 
-int Taille_Mots(char*);
+L_SOMMET* Creer_Graphe(FILE* fichier,int taille_mots, int nombre_mots);
+
+int Taille_Mots(FILE*);
+int Taille_Fichier(FILE* fichier); 
+void Affiche(char* chaine);
+Liste Creer_Liste(void);
+int Est_Vide(Liste L);
+//Liste rech(ELEMENT e, Liste L);
+L_SUCC Rech_Chaine(char* chaine, L_SOMMET L);
+T_SUCC Ajout_Tete(char* e, L_SUCC L,int taille_mots);
+L_SUCC Ajout_Tete_L_SUCC(T_SUCC e, L_SUCC L);
+int Taille_Liste(Liste l);
+void Visualiser_Liste(Liste L);
+// d'autre fnctions de listes sont dispo dans le TD9
 
 #endif /* defined(____fonctions__) */
