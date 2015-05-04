3cb880b1ef215dff25490be4d51970bcadfa9952
Projet-info-Ma-Pich <lucas.mahieu@icloud.com>
2015-05-04 19:10:22 +0200

    Update Test_Table_Hashage.c
    maj 4/5

--- Sources/Test_Table_Hashage.c
+++ Sources/Test_Table_Hashage.c
@@ -4,21 +4,33 @@
 int main(int argc,char** argv){ // le main va prendre 2 arg. le fichier de mots et la tailles max des mots
   	T_GRAPHE G;
   	int n;
-	if (argc<1) {
-		return 0;
-	}
-	int taille_mots = Taille_Mots(argv[1]);
-	G.table=creer_graphe(argv[1],&(G.taille),taille_mots);  
+//	if (argc<1) {
+//		return 0;
+//	}
+	FILE* f1;
+	if( ( f1=fopen("test_mot3.txt","r") ) == NULL ) return 1;
+	
+	int taille_mots = Taille_Mots(f1);
+	int nombre_mots = Taille_Fichier(f1);
+	
+	printf("test0 \n");
+	printf("taille des mots = %d \n",taille_mots);
+	G.table=Creer_Graphe(f1,taille_mots,nombre_mots);
+	printf("est 2 \n");
+	G.taille=Taille_Fichier(f1);
+	
+	
+	printf("test1 \n");
 	
 	  while(1){
 	
     	printf("Quelle position dans la table ? \n");
     	scanf("%d",&n); /*On se a place à un certain point dans la table de hashage et on affiche l'ensemble de la liste associée*/
 
-     visualiser_liste(G.table+n);
+     Visualiser_Liste(*(G.table+n));
      
     getchar();
 	  }
+	if((fclose(f1))==0)  return 1; 
 	return 0;
 }
-
